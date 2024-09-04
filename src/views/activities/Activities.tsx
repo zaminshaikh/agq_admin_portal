a06202bf4b954110dea4a31bcab20c3c4eb31637
Zamin Shaikh <zaminusa@gmail.com>
2024-09-04 19:15:12 -0400

    removed fetch in Activities

--- src/views/activities/Activities.tsx
+++ src/views/activities/Activities.tsx
@@ -6,27 +6,6 @@ import { CSpinner } from "@coreui/react-pro";
 
 const Activities = () => {
     const { t } = useTranslation()
-    const [user, setUser] = useState<User>();
-    const [users, setUsers] = useState<User[]>([]);
-    const [isLoading, setIsLoading] = useState(true);
-    
-    useEffect(() => {
-        const fetchActivities = async () => {
-            const db = new DatabaseService();
-            const users = await db.getUsers();
-            setUsers(users);
-            setIsLoading(false);
-        };
-        fetchActivities();
-    }, []);
-    
-    if (isLoading) {
-        return( 
-            <div className="text-center">
-                <CSpinner color="primary"/>
-            </div>
-        )
-    }
 
     return (
         <div>
