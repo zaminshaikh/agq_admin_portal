d8fb2352d622318765c88537020b90ddc8bd175f
Zamin Shaikh <zaminusa@gmail.com>
2026-03-02 20:12:24 -0500

    Disable Prettier rule in ESLint configuration to allow for more flexible code formatting.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,6 +24,7 @@ module.exports = {
     },
   },
   rules: {
+    'prettier/prettier': 'off',
     '@typescript-eslint/no-unused-vars': 'error',
     '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
